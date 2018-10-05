b211d27a3f16afee56322a1ed05f5231b0eb9243
Pradyumn Singh <22pradyumns@gmail.com>
2018-10-05 16:27:31 +0000

    adding edit and show pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,37 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @articles = Article.new(article_params)
-    @articles.save
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+      
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+      
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
-    def article_params
+  def article_params
       params.require(:article).permit(:title, :description)
-    end
+  end
 end
